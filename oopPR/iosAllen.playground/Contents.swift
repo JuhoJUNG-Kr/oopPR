223f0ecbbb309e3e33b56e1cdd6c69eed1eeb3b4
Juho Jung <jooho293@gmail.com>
2022-04-20 16:29:14 +0200

    Feat: fucntion
    -

--- oopPR/iosAllen.playground/Contents.swift
+++ oopPR/iosAllen.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Darwin
 
 //for i in 2...9 {
 //    for j in 1...9 {
@@ -24,7 +25,7 @@ import UIKit
 //    print()
 //}
 //
-
+//
 //func body(large: Int, long: Int) {
 //    var shape = "*"
 //    for _ in 1...long {
@@ -44,46 +45,120 @@ import UIKit
 //
 //body(large: 11, long: 8)
 //stick(long: 4)
+//
+//let nudePpePero = " | | "
+//
+//
+//func stick(long: Int) {
+//    for _ in 1...long {
+//        print(nudePpePero)
+//    }
+//}
+//
+//func body(shape: String, topping: String) {
+//    print("\(topping)\(shape)\(topping)")
+//}
+//
+//func bodyLong(long: Int, shape: String, topping: String) {
+//    if shape == "***" {
+//        for _ in 1...long {
+//            body(shape: shape, topping: topping)
+//        }
+//    } else {
+//        for count in 1...long {
+//            if count % 2 != 0 {
+//                print(nudePpePero)
+//            } else {
+//                print(" |0| ")
+//            }
+//        }
+//    }
+//}
+//func order(stickLong: Int, long: Int, shape: String, topping: String) {
+//
+//    print("""
+//<정보>
+//길이: \(long)
+//몸통: \(shape)
+//토핑: \(topping)
+//막대길이: \(stickLong)
+//""")
+//    print()
+//
+//    bodyLong(long: long, shape: shape, topping: topping)
+//    stick(long: stickLong)
+//}
+//
+//order(stickLong: 4, long: 6, shape: nudePpePero, topping: " ")
+//
+//
+//func pickUpLatter(a: String) -> String {
+//    return String(a.randomElement()!)
+//}
+//
+//pickUpLatter(a: "안녕하세요")
+//
+//var num = 3
+//
+//var isPrime: Bool = true
+//
+//for i in 2..<num {
+//    if num % i == 0 {
+//        isPrime = false
+//        break
+//    }
+//}
+//
+//if isPrime {
+//    print("소수입니다.")
+//} else {
+//    print("소수가 아닙니다.")
+//}
+//
+//func checkPrimeNumbers(num: Int) -> Bool{
+//    for i in 2..<num {
+//        if num % i == 0 {
+//            return false
+//        }
+//    }
+//    return true
+//}
+//
+//if checkPrimeNumbers(num: 7) {
+//    print("소수입니다.")
+//} else {
+//    print("소수가 아닙니다.")
+//}
+//
+//
+//func primeNumbers(num: Int) {
+//    for i in 2..<num {
+//        if num % i == 0 {
+//            print("소수가 아닙니다.")
+//            return
+//        }
+//    }
+//    print("소수입니다.")
+//}
+//
+//primeNumbers(num: 6)
 
+//func pack(num: Int) -> Int {
+//    var sum = 1
+//
+//    for i in 1...num {
+//        sum = sum * i
+//    }
+//    return sum
+//}
+//
+//pack(num: 5)
 
-
-func stick(long: Int) {
-    let shape = " | |  "
-    for _ in 1...long {
-        print(shape)
+func facktorial(num: Int) -> Int {
+    if num <= 1 {
+        return 1
     }
+    return num * facktorial(num: num - 1)
 }
 
-func body(shape: String, topping: String) {
-    print("\(topping)\(shape)\(topping)")
-}
-
-func bodyLong(long: Int, shape: String, topping: String) {
-    if shape == "***" {
-        for _ in 1...long {
-            body(shape: shape, topping: topping)
-        }
-    } else {
-        for count in 1...long {
-            if count % 2 != 0 {
-                print(" |0| ")
-            }
-        }
-    }
-}
-func order(stickLong: Int, long: Int, shape: String, topping: String) {
-    
-    print("""
-<정보>
-길이: \(long)
-몸통: \(shape)
-토핑: \(topping)
-막대길이: \(stickLong)
-""")
-    print()
-    
-    bodyLong(long: long, shape: shape, topping: topping)
-    stick(long: stickLong)
-}
-
-order(stickLong: 4, long: 6, shape: " | | ", topping: " ")
+facktorial(num: 10)
